7b414761982935750196750bf655516182ce7e10
Juan Bosco Mendoza Vega <jboscomendoza@gmail.com>
2023-01-02 17:08:17 -0600

    Scrape superpowers

--- 00_initial_scrape.py
+++ 00_initial_scrape.py
@@ -5,9 +5,27 @@ import requests
 # https://www.mediawiki.org/wiki/API:Presenting_Wikidata_knowledge#Parsing_claims
 # https://www.wikidata.org/w/api.php?action=help&modules=wbgetentities
 
-base_url = "http://www.wikidata.org/w/api.php?{params}&format=json"
-params_query = "action=query&list=search&srsearch=Jubilee Marvel"
-query_url = base_url.format(params=params_query)
+search_term = "Jubilee Marvel"
+
+base_url = "http://www.wikidata.org/w/api.php?format=json"
+query_params = "&action=query&list=search&srsearch="
+query_url = base_url + query_params + search_term
+
+params_str = (
+    "&action=wbgetentities" +
+    "&sites=wikidatawiki" +
+    "&languages=es" +
+    "&languagefallback=true" +
+    "&sitefilter=eswiki" +
+    "&ids="
+    )
+
+claims = {
+    "genero":"P21",
+    "superpoder":"P2563",
+    "membresia":"P463",
+    "universo":"P1080"
+    }
 
 resp_query = requests.get(query_url)
 
@@ -17,33 +35,33 @@ results = []
 
 for i in query_json["query"]["search"]:
    results.append(dict(title=i["title"], pagid=i["pageid"]))
-   
-char_id = results[0]["title"]
-params_get = "action=wbgetentities&sites=wikidatawiki&languages=es&props=aliases|claims|sitelinks&sitefilter=eswiki&ids={}".format(char_id)
-char_url = base_url.format(params=params_get)
 
-
-resp_char = requests.get(char_url)
-resp_dat = resp_char.json()
-
-char_id = resp_dat["entities"][char_id]["id"]
-char_title = resp_dat["entities"][char_id]["sitelinks"]["eswiki"]["title"]
-
-# Prop id
-claims = {
-    "genero":"P21",
-    "superpoder":"P2563",
-    "membresia":"P463",
-    "universo":"P1080"
-    }
-
+char_id    = results[0]["title"]
+char_url   = base_url + params_str + char_id
+char_resp  = requests.get(char_url)
+char_json  = char_resp.json()
+char_title = char_json["entities"][char_id]["sitelinks"]["eswiki"]["title"]
 char_claims = []
 
+
 for claim in claims.values():
-    snaks = resp_dat["entities"][char_id]["claims"].get(claim)
+    snaks = char_json["entities"][char_id]["claims"].get(claim)
     for snak in snaks:
         key_info = {
-            "property":snak["mainsnak"]["property"],
-            "id":snak["mainsnak"]["datavalue"]["value"]["id"]
+            snak["mainsnak"]["property"]: snak["mainsnak"]["datavalue"]["value"]["id"]
         }
         char_claims.append(key_info)
+    
+sp_value = []
+
+for i in char_claims:
+    sp_id = i.get(claims["superpoder"])
+    if sp_id is not None:
+        sp_value.append(sp_id)
+
+sp_id = "|".join(sp_value)
+sp_url = base_url + params_str + sp_id
+sp_resp = requests.get(sp_url)
+sp_json = sp_resp.json()
+sp_data = sp_json["entities"][sp_id]["labels"]["es"]["value"]
+sp_desc = sp_json["entities"][sp_id]["descriptions"]["es"]["value"]
