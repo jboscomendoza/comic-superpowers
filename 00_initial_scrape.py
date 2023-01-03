337b6a4bce729515b4128edd74575ea08a72f19a
Juan Bosco Mendoza Vega <jboscomendoza@gmail.com>
2023-01-03 00:06:42 -0600

    Obtener superpoderes como funcion

--- 00_initial_scrape.py
+++ 00_initial_scrape.py
@@ -5,11 +5,12 @@ import requests
 # https://www.mediawiki.org/wiki/API:Presenting_Wikidata_knowledge#Parsing_claims
 # https://www.wikidata.org/w/api.php?action=help&modules=wbgetentities
 
-search_term = "Jubilee Marvel"
-
+search_term = "Emma frost Marvel"
+ 
 base_url = "http://www.wikidata.org/w/api.php?format=json"
-query_params = "&action=query&list=search&srsearch="
+query_params = "&action=query&list=search&srpop&srsearch="
 query_url = base_url + query_params + search_term
+
 
 params_str = (
     "&action=wbgetentities" +
@@ -19,49 +20,95 @@ params_str = (
     "&sitefilter=eswiki" +
     "&ids="
     )
-
-claims = {
+ 
+CLAIMS = {
     "genero":"P21",
     "superpoder":"P2563",
     "membresia":"P463",
     "universo":"P1080"
     }
-
+ 
 resp_query = requests.get(query_url)
-
+ 
 query_json = resp_query.json()
+ 
+def get_char_json(json_data):
+    results = []
+    for i in json_data["query"]["search"]:
+        char_id = i["title"]
+        char_url   = base_url + params_str + char_id
+        char_resp  = requests.get(char_url)
+        char_json  = char_resp.json()
+        char_json["id"] = char_id
+        results.append(char_json)
+    return results
+       
+results  = get_char_json(query_json)
 
-results = []
+def get_entity_json(char_json):
+    entity_id = char_json["id"]
+    entity_url = base_url + params_str + entity_id
+    entity_resp = requests.get(entity_url)
+    entity_json = entity_resp.json()
+    entity_json["id"] = entity_id
+    return entity_json
 
-for i in query_json["query"]["search"]:
-   results.append(dict(title=i["title"], pagid=i["pageid"]))
+entities = [get_entity_json(i) for i in results]
 
-char_id    = results[0]["title"]
-char_url   = base_url + params_str + char_id
-char_resp  = requests.get(char_url)
-char_json  = char_resp.json()
-char_title = char_json["entities"][char_id]["sitelinks"]["eswiki"]["title"]
+
+entity_title = (
+    entities[1]
+    .get("entities")
+    .get(entities[1].get("id"))
+    .get("sitelinks")
+    .get("eswiki", {})
+    .get("title", "None")
+)
+
 char_claims = []
 
-
-for claim in claims.values():
-    snaks = char_json["entities"][char_id]["claims"].get(claim)
+for claim_k, claim_v in CLAIMS.items():
+    snaks = (
+        entities[1]
+        .get("entities")
+        .get(entities[1].get("id"))
+        .get("claims")
+        .get(claim_v)
+    )
     for snak in snaks:
         key_info = {
-            snak["mainsnak"]["property"]: snak["mainsnak"]["datavalue"]["value"]["id"]
+            claim_k: (
+                snak
+                .get("mainsnak")
+                .get("datavalue")
+                .get("value")
+                .get("id")
+            )
         }
         char_claims.append(key_info)
+        
+char_claims
+
+sp_value = [i.get("superpoder") for i in char_claims if i.get("superpoder") is not None]
+sp_value
+
+def get_sp_json(sp_list):
+    sp_ids = "|".join(sp_list)
+    sp_url = base_url + params_str + sp_ids
+    sp_resp = requests.get(sp_url)
+    sp_json = sp_resp.json()
+    return sp_json
     
-sp_value = []
+def get_sp_data(sp_id, sp_json):
+    sp_entity = sp_json.get("entities").get(sp_id)
+    sp_dict = dict(
+        sp_data = sp_entity.get("labels").get("es").get("value"),
+        sp_id = sp_entity.get("descriptions").get("es").get("value")
+        )
+    return sp_dict
 
-for i in char_claims:
-    sp_id = i.get(claims["superpoder"])
-    if sp_id is not None:
-        sp_value.append(sp_id)
+sp_json = get_sp_json(sp_value)
+sp_data = [get_sp_data(i, sp_json) for i in sp_value]
+sp_data
 
-sp_id = "|".join(sp_value)
-sp_url = base_url + params_str + sp_id
-sp_resp = requests.get(sp_url)
-sp_json = sp_resp.json()
-sp_data = sp_json["entities"][sp_id]["labels"]["es"]["value"]
-sp_desc = sp_json["entities"][sp_id]["descriptions"]["es"]["value"]
+
