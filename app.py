88bdd97871d3284f3d1cab8de60f33899d727b98
Juan Bosco Mendoza Vega <jboscomendoza@gmail.com>
2023-01-12 13:09:27 -0600

    Plot tab personaje con plotly

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import pandas as pd
+from plotly import graph_objects as go
 
-cols_str = ["char_desc", "sp_nombre", "sp_desc", "team_desc"]
+cols_str = ["char_desc", "sp_nombre", "sp_desc", "team_desc", "gen_nombre"]
 
 char = pd.read_parquet("char_data.parquet")
 char[cols_str] = char[cols_str].apply(lambda x: x.str.capitalize())
@@ -22,6 +23,7 @@ conteo_sp = (
 conteo_sp.columns = ["Superpoder", "Conteo"]
 
 st.markdown("## Superpoderes más frecuentes")
+st.metric("Superpoderes distintos", len(conteo_sp))
 st.bar_chart(conteo_sp, x="Superpoder", y="Conteo")
 
 
@@ -35,6 +37,7 @@ conteo_team = (
 )
 conteo_team.columns = ["Equipo", "Conteo"]
 st.markdown("## Equipos más frecuentes")
+st.metric("Equipos distintos", len(conteo_team))
 st.bar_chart(conteo_team, x="Equipo", y="Conteo")
 
 
@@ -53,6 +56,20 @@ tab_team.table(char_team)
 
 
 # Personaje
+conteo_gen = char[["char_nombre", "gen_nombre"]].drop_duplicates().groupby("gen_nombre", as_index=False).count()
+gen_bar = go.Figure()
+gen_bar.add_trace(go.Bar(   
+    x=conteo_gen["gen_nombre"],
+    y=conteo_gen["char_nombre"]
+))
+gen_bar.update_layout(dict(
+     height=200, 
+     margin=dict(t=25, r=0, b=0, l=0),
+     ))
+
+tab_char.metric("Personajes distintos", len(char["char_nombre"].unique()))
+tab_char.plotly_chart(gen_bar)
+
 sel_char = tab_char.selectbox(
     "Elige un personaje",
     char["char_nombre"].unique()
@@ -60,16 +77,16 @@ sel_char = tab_char.selectbox(
 
 pers = char.loc[char["char_nombre"] == sel_char]
 
-p_1, p_2, p_3 = tab_char.columns(3)
+tab_char.markdown(pers["char_desc"].unique().item())
 
-p_1.markdown("### Súperpoderes")
-for sp in pers["sp_nombre"].drop_duplicates():
-    p_1.markdown(sp)
+char_cols = tab_char.columns(3)
+contenido = {
+    "Súperpoderes": pers["sp_nombre"].drop_duplicates(),
+    "Equipos": pers["team_nombre"].drop_duplicates(),
+    "Universos": pers["uni_nombre"].drop_duplicates()
+}
 
-p_2.markdown("### Equipos")
-for team in pers["team_nombre"].drop_duplicates():
-    p_2.markdown(team)
-    
-p_3.markdown("### Universos")
-for uni in pers["uni_nombre"].drop_duplicates():
-    p_3.markdown(uni)
+for i_col, i_key, i_value in zip(char_cols, contenido.keys(), contenido.values()):
+    i_col.markdown("### "+i_key)
+    for value_in_v in i_value:
+        i_col.markdown(value_in_v)
