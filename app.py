9ebd4e8da5c92118aa41953a767f2a6a4ebb4c58
Juan Bosco Mendoza Vega <jboscomendoza@gmail.com>
2023-01-12 17:01:31 -0600

    Ajustes de formato

--- app.py
+++ app.py
@@ -7,10 +7,8 @@ cols_str = ["char_desc", "sp_nombre", "sp_desc", "team_desc", "gen_nombre"]
 char = pd.read_parquet("char_data.parquet")
 char[cols_str] = char[cols_str].apply(lambda x: x.str.capitalize())
 
-### ###
-### App
-### ###
-st.markdown("# X-Men")
+
+char_unique = char["char_nombre"].unique()
 
 conteo_sp = (
     char[["char_nombre", "sp_nombre"]]
@@ -32,18 +30,31 @@ conteo_team = (
 )
 conteo_team.columns = ["Equipo", "Conteo"]
 
-char_unique = char["char_nombre"].unique()
+conteos = {
+    "Personajes": len(char_unique), 
+    "Poderes": len(conteo_sp),
+    "Equipos": len (conteo_team)
+}
 
-conteos = [char_unique, conteo_sp, conteo_team]
 
-for cont, cont_col in zip(conteos, st.columns(len(conteos))):
-    cont_col.metric("Personajes distintos", len(cont))
+### ###
+### App
+### ###
+st.markdown("# X-Men")
+
+metric_cols = st.columns(len(conteos))
+
+for cont_k, cont_v, cont_col in zip(conteos.keys(), conteos.values(), metric_cols):
+    cont_col.metric(cont_k, cont_v)
 
 
 # Tabs
-tab_sp, tab_team, tab_char = st.tabs(["Poderes", "Equipos", "Personajes"])
+tab_sp, tab_team, tab_char = st.tabs([
+    ":mortar_board: Poderes", 
+    ":globe_with_meridians: Equipos", 
+    ":busts_in_silhouette: Personajes"])
 
-
+## Poder
 sp_bar = go.Figure()
 sp_bar.add_trace(go.Bar(   
     x=conteo_sp["Superpoder"].str.slice(0, 20),
@@ -54,15 +65,27 @@ sp_bar.update_layout(dict(
      margin=dict(t=25, r=0, b=0, l=0),
      ))
 
-tab_sp.markdown("## Superpoderes más frecuentes")
+tab_sp.markdown("## Poderes más frecuentes")
 tab_sp.plotly_chart(sp_bar)
 
-sel_sp = tab_sp.selectbox("Elige un superpoder", char["sp_nombre"].sort_values().unique())
-char_sp = char["char_nombre"].loc[char["sp_nombre"] == sel_sp].drop_duplicates()
-tab_sp.metric("Personajes", len(char_sp))
-tab_sp.table(char_sp)
+sel_sp = tab_sp.selectbox("Elige un poder", char["sp_nombre"].sort_values().unique())
 
-# Equipos
+sp_desc = char["sp_desc"].loc[char["sp_nombre"] == sel_sp].unique().item()
+tab_sp.markdown(sp_desc+".")
+
+char_sp = char["char_nombre"].loc[char["sp_nombre"] == sel_sp].drop_duplicates()
+tab_sp.metric("Personajes con este poder", len(char_sp))
+char_sp_nombres = (
+    char_sp
+    .to_frame()
+    .sort_values("char_nombre")
+    .reset_index(drop=True)
+    .rename(columns={"char_nombre":"Personaje"})
+    )
+tab_sp.table(char_sp_nombres)
+
+
+## Equipos
 team_bar = go.Figure()
 team_bar.add_trace(go.Bar(   
     x=conteo_team["Equipo"].str.slice(0, 20),
@@ -79,12 +102,23 @@ tab_team.plotly_chart(team_bar)
 
 
 sel_team = tab_team.selectbox("Elige un equipo", char["team_nombre"].sort_values().unique())
+
+team_desc = char["team_desc"].loc[char["team_nombre"] == sel_team].unique().item()
+tab_team.markdown(team_desc+".")
+
 char_team = char["char_nombre"].loc[char["team_nombre"] == sel_team].drop_duplicates()
-tab_team.metric("Personajes", len(char_team))
-tab_team.table(char_team)
+tab_team.metric("Personajes que han sido integrantes de este equipo", len(char_team))
+char_team_nombres = (
+    char_team
+    .to_frame()
+    .sort_values("char_nombre")
+    .reset_index(drop=True)
+    .rename(columns={"char_nombre":"Personaje"})
+    )
+tab_team.table(char_team_nombres)
 
 
-# Personaje
+# Personajes
 conteo_gen = char[["char_nombre", "gen_nombre"]].drop_duplicates().groupby("gen_nombre", as_index=False).count()
 gen_bar = go.Figure()
 gen_bar.add_trace(go.Bar(   
@@ -106,14 +140,16 @@ sel_char = tab_char.selectbox(
 
 pers = char.loc[char["char_nombre"] == sel_char]
 
-tab_char.markdown(pers["char_desc"].unique().item())
+char_desc = pers["char_desc"].unique().item()
+tab_char.markdown(char_desc+".")
 
-char_cols = tab_char.columns(3)
 contenido = {
-    "Súperpoderes": pers["sp_nombre"].drop_duplicates(),
+    u"Género": pers["gen_nombre"].drop_duplicates(),
+    "poderes": pers["sp_nombre"].drop_duplicates(),
     "Equipos": pers["team_nombre"].drop_duplicates(),
     "Universos": pers["uni_nombre"].drop_duplicates()
 }
+char_cols = tab_char.columns(len(contenido))
 
 for i_col, i_key, i_value in zip(char_cols, contenido.keys(), contenido.values()):
     i_col.markdown("### "+i_key)
